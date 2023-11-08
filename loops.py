15c9a42cef783afb6bc878129ee6eb3e8b531ad3
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2023-11-07 19:40:05 -0600

    learning loops

---
+++ loops.py
@@ -0,0 +1,15 @@
+def loop(t):
+    for i in t:
+        print(i)
+        
+def whileloop():
+    c=0
+    while(c<5):
+        c= c+1
+        print(c)
+
+        
+l = {"printing", "as many", "stuff"}
+
+whileloop()
+loop(l)
